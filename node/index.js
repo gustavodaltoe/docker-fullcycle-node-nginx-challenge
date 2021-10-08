bd0ba6c6a432d38dbfe053dc20532ac6c6038a23
root <root@DESKTOP-Q1IUDU5.localdomain>
2021-10-07 22:48:14 -0300

    docker compose and js code

---
+++ node/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+
+const sql = `INSERT INTO people(name) values('Gustavo'), ('Ana'), ('Mandy')`
+connection.query(sql)
+
+app.get('/', (req,res) => {
+  const connection = mysql.createConnection(config)
+  const query = 'SELECT name FROM people'
+  connection.query(query, (err, results) => {
+    if (err) throw err;
+
+    const names = results.map(({ name }) => '- ' + name).join('<br>')
+
+    res.send(`<h1>Full Cycle</h1><br><br>${names}`)
+  })
+
+  connection.end()
+})
+
+app.listen(port, ()=> {
+  console.log(`App is running on port ${port}`)
+})
